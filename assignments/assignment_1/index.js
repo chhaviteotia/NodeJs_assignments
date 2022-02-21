103427fd85341c313e5a041acb7fc51c0899919d
chhaviteotia <chhavi.teotia8@gmail.com>
2022-02-21 13:22:38 +0530

    nodeJs_asgnmt_1

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,13 +1,28 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+    const name=process.argv
+    return (name[name.length-1])
+
 }
 
 function getNameFromEnv() {
     // Write your code here
+    return process.env.name;
+
 }
 
 function getNameFromReadLine() {
     // Write your code here
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input:process.stdin,
+        output: process.stdout
+    });
+    rl.question('What is your name', (name)=>{
+        return name
+        rl.close();
+    });
+    
 }
 
 module.exports = {
